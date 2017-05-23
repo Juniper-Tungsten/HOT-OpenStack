86f8c23b228ab3b2307804f01e4eb079a1e669f1
Savvy_Through <savithruml@users.noreply.github.com>
2017-05-22 23:32:16 -0700

    Update sanity.sh

--- sanity/sanity.sh
+++ sanity/sanity.sh
@@ -7,6 +7,7 @@ glance image-create --name ubuntu --visibility=public --container-format ovf --d
 wget http://10.84.5.120/cs-shared//images/vsrx/junos-vsrx-12.1-in-network.img.gz
 gunzip junos-vsrx-12.1-in-network.img.gz
 glance image-create --name nat-service --visibility=public --container-format ovf --disk-format qcow2 --file junos-vsrx-12.1-in-network.img
+git clone https://github.com/savithruml/HOT-OpenStack
 heat stack-create sanity-check-sc -f /root/HOT-OpenStack/templates/nat-svc-stack-v2.hot -e /root/HOT-OpenStack/env/nat-svc-stack-v2.env
 
 until heat stack-list | grep -i "complete" &> /dev/null; do sleep 2 ; done; echo "TEST PASS"
